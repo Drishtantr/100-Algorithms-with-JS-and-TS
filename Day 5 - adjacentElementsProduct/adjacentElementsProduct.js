2ea81734fdbbe4e58382a2f19c1f36c163769de1
drishtantr <drishtantregmi777@gmail.com>
2020-10-03 07:49:05 +0545

    Day 5

---
+++ Day 5 - adjacentElementsProduct/adjacentElementsProduct.js
@@ -0,0 +1,10 @@
+function adjacentElementsProduct(a) {
+    var largest = a[0] * a[1];
+    for(var i=0; i<a.length-1; i++){
+        const p = a[i] *a[i+1]
+        largest = p > largest ? p : largest;
+    }
+    return largest;
+}
+
+console.log(adjacentElementsProduct([1,5,20,2,3]));
