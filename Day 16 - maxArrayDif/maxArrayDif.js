1255f4f90fe9a5b99d97535af72c590deca2a3a9
drishtantr <drishtantregmi777@gmail.com>
2020-10-14 07:01:11 +0545

    Day 16

---
+++ Day 16 - maxArrayDif/maxArrayDif.js
@@ -0,0 +1,10 @@
+//max sum of 2 consecutive elements
+function maxArrayDiff(a) {
+    var min= Math.abs(a[0] - a[1]);
+    for (var i=0; i<a.length-1; i++) {
+        min = Math.abs(a[i]-a[i+1]) > min ? Math.abs(a[i]-a[i+1]): min;
+    }
+    return min;
+};
+
+console.log(maxArrayDiff([2,3,5,1,6]));
---
+++ Day 16 - maxArrayDif/maxArrayDif.ts
@@ -0,0 +1,10 @@
+//max sum of 2 consecutive elements
+function maxArrayDiffts(a: number[]): number {
+    var min= Math.abs(a[0] - a[1]);
+    for (var i=0; i<a.length-1; i++) {
+        min = Math.abs(a[i]-a[i+1]) > min ? Math.abs(a[i]-a[i+1]): min;
+    }
+    return min;
+};
+
+console.log(maxArrayDiffts([2,3,5,1,16]));
