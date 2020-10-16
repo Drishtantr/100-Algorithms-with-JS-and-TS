4da1e5f17a254e941dbb8495f1a059693b2afa17
drishtantr <drishtantregmi777@gmail.com>
2020-10-16 06:33:35 +0545

    Day 18

---
+++ Day 18 - elementReplace/elementReplace.js
@@ -0,0 +1,10 @@
+function elementReplace(a, ele, sub) {
+    for(let i=0; i<a.length; i++) {
+        if (a[i]===ele) {
+            a[i]=sub;
+        }
+    }
+    return a;
+};
+
+console.log(elementReplace([1,2,1],1,3));
