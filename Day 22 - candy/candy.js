3f3f933dbb79e17b01af89bbb4d741e3d76f370a
drishtantr <drishtantregmi777@gmail.com>
2020-10-20 07:39:50 +0545

    Day 22

---
+++ Day 22 - candy/candy.js
@@ -0,0 +1,5 @@
+function candy(n, m) {
+    return n*Math.floor(m/n);    
+};
+
+console.log(candy(4, 10));
---
+++ Day 22 - candy/candy.ts
@@ -0,0 +1,5 @@
+function candyts(n: number, m: number): number {
+    return n*Math.floor(m/n);    
+};
+
+console.log(candy(4, 10));
