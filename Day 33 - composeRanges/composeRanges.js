c0c4a3c54e04dd9eeb56a6056be239da5cd5c83f
drishtantr <drishtantregmi777@gmail.com>
2020-10-31 08:11:46 +0545

    Day 33

---
+++ Day 33 - composeRanges/composeRanges.js
@@ -0,0 +1,22 @@
+function composeRanges(nums) {
+    var range = [{start: nums[0], end: nums[0]}];
+    console.log(range);
+    for(let i=1; i<nums.length; i++ ) {
+        if (range[range.length -1].end +1 === nums[i]) {
+            range[range.length -1].end = nums[i];
+        } else {
+            range.push({start: nums[i], end: nums[i]});
+        }
+    }
+    console.log(range);
+    for (let i=0; i<range.length; i++) {
+        if(range[i].start !== range[i].end) {
+            range[i] = range[i].start + '->' + range[i].end;
+        } else {
+            range[i] = range[i].end.toString()
+        }
+    }
+    return range;
+};
+
+console.log(composeRanges([-1,0,1,2,6,7,9]));
