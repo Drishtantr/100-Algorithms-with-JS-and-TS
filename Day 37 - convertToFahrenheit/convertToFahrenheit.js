c4c67ea413c11179f521ea9d8558615d6beb996a
drishtantr <drishtantregmi777@gmail.com>
2020-11-04 07:17:40 +0545

    Day 37

---
+++ Day 37 - convertToFahrenheit/convertToFahrenheit.js
@@ -0,0 +1,7 @@
+function convertToFahrenheit(cel) {
+    return (cel * 9/5) + 32;
+};
+
+console.log(convertToFahrenheit(-30));
+console.log(convertToFahrenheit(-10));
+console.log(convertToFahrenheit(0));
---
+++ Day 37 - convertToFahrenheit/convertToFahrenheit.ts
@@ -0,0 +1,7 @@
+function convertToFahrenheitts(cel: number): number {
+    return (cel * 9/5) + 32;
+};
+
+console.log(convertToFahrenheitts(-30));
+console.log(convertToFahrenheitts(-10));
+console.log(convertToFahrenheitts(0));
