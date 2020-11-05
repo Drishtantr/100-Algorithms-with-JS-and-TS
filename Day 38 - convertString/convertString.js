91580cb21ca5ff10fb39808261ba5f5c47e8c352
drishtantr <drishtantregmi777@gmail.com>
2020-11-05 08:11:52 +0545

    Day 38

--- Day 38 - convertString/convertString.js
+++ Day 38 - convertString/convertString.js
@@ -16,3 +16,4 @@ function convertString(s, t) {
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
 console.log(convertString('addbyca', 'abcd'));
+console.log(convertString('akjpdfefpdfewlsde', 'apple'));
