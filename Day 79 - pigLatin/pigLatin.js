57f96dbd06fbb08596a4ed99901183c05498aae9
Drishtant Regmi <drishtantregmi777@gmail.com>
2020-12-16 10:29:12 +0545

    Day79

---
+++ Day 79 - pigLatin/pigLatin.js
@@ -0,0 +1,24 @@
+function pigLatin(str) {
+    const lettere = str.split('');
+    const vowel = /[aeiou]/;
+
+    if (vowel.test(str[0])) {
+        return `${str}way`;
+    }
+
+    while (true) {
+        if (!vowel.test(lettere[0])) {
+            const firstCo = lettere.splice(0,1)[0]
+            lettere.push(firstCo);
+        } else {
+            break;
+        }
+    }
+
+    str = lettere.join('') + 'ay';
+    return str;
+
+}
+
+console.log(pigLatin('glove'));
+console.log(pigLatin('eight'));
