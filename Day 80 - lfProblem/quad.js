b9e2b48385e2bac97c9b482348930b41f3fa8c10
Drishtant Regmi <drishtantregmi777@gmail.com>
2020-12-17 20:57:42 +0545

    Day 80

--- Day 80 - lfProblem/quad.js
+++ Day 80 - lfProblem/quad.js
@@ -2,10 +2,18 @@ function quad(a) {
     let leng = Math.floor(a.length/4)
     console.log(leng);
     let neww = ''
+    const divv = a.length%4;
+    console.log(divv);
     for (let i=0; i<a.length; i+=4) {
-        neww = neww + `${a[i+2]}${a[i+3]}${a[i]}${a[i+1]}`
+        if (a[i+3] === undefined) {
+            for(let k=0; k<divv; k++){
+                neww += a[k+i];
+            }
+        } else {
+            neww = neww + `${a[i+2]}${a[i+3]}${a[i]}${a[i+1]}`
+        }
     }
     return neww;
 }
 
-console.log(quad('ABCDEFGH'));
+console.log(quad('ABCDEFGHIJ'));
