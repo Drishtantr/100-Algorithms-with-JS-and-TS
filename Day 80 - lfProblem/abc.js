dcf97f74998c7c019a0b2090ebf4930771f1db3c
Drishtant Regmi <drishtantregmi777@gmail.com>
2020-12-17 15:38:27 +0545

    Day 80

--- Day 80 - lfProblem/abc.js
+++ Day 80 - lfProblem/abc.js
@@ -1,10 +1,11 @@
-function findd(n) {
-    const sqr = Math.sqrt(n);
+function findd(n,p) {
+    const sqr = Math.pow(n, 1/p)
+    console.log(sqr);
     if (n%sqr === 0) {
         return 1;
     }
     return 0;
 }
 
-console.log(findd(16));
-console.log(findd(6));
+console.log(findd(16,2));
+console.log(findd(27, 2));
